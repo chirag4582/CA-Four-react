5a5d83990ea16b077fc9bf5f0a1426c95ba80e19
chirag4851 <chiragchavda1243@gmail.com>
2022-12-30 13:05:19 +0530

    removed eslint warnings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import questions from "./questions";
-import Result from "./components/Result";
+// import questions from "./questions";
+// import Result from "./components/Result";
 import QuestionBox from "./components/QuestionBox";
 
 
--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import questions from "../questions";
 import Result from "./Result";
 function QuestionBox() {
